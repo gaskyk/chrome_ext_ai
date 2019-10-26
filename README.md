11d40bb4811b772afefa57a21deb546ae3188e4d
Karen (Gask) Walker <gaskyk@users.noreply.github.com>
2019-10-26 18:23:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AI is fancy maths - a Chrome extension
+
+A Chrome extension that changes all instances on a web page of AI or artificial intelligence to "fancy maths". Because that is what AI is.
---
+++ content.js
@@ -0,0 +1,27 @@
+/* Create Chrome extension to replace AI with fancy maths */
+
+
+// This makes an array of everything inside the body tag
+var elementsInsideBody = [...document.body.getElementsByTagName('*')];
+
+// A function that loops through every single item
+function findAndReplace(){
+  elementsInsideBody.forEach(element =>{
+    element.childNodes.forEach(child =>{
+      if(child.nodeType === 3){
+        replaceText(child);
+      }
+    });
+
+  });
+}
+
+function replaceText (node) {
+  let value = node.nodeValue;
+  value = value.replace(/ AI /gi, ' fancy maths ');
+  value = value.replace(/Artificial intelligence/gi, 'Fancy maths');
+  value = value.replace(/artificial intelligence/gi, 'fancy maths');
+  node.nodeValue = value;
+}
+
+window.onload = findAndReplace();
